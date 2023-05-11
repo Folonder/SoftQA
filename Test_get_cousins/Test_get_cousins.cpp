a01c0dbe6f1a2485efe996cbde4628fffc655f6e
Folonder <olroldugin@yandex.ru>
2023-05-11 21:18:18 +0300

    cleared the repository of unnecessary files

---
+++ Test_get_cousins/Test_get_cousins.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testgetcousins
+{
+	TEST_CLASS(Testgetcousins)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
